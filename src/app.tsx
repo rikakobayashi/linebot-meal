afe78e38e207f6605bab2802994549c7c2424019
小林莉華 <candypops0530@yahoo.co.jp>
2020-04-12 18:29:40 +0900

    add notEatout style and calendar info

--- src/app.tsx
+++ src/app.tsx
@@ -78,13 +78,26 @@ class App extends React.Component<AppProps, AppState> {
     this.postMyData(!isDayHighlighted, date)
   }
 
-  isDayHighlighted = (date: moment.Moment, allData: eatOutData[]) => {
-    const eatOutData = allData.find(data => {
+  getEatOutData = (date: moment.Moment, allData: eatOutData[]) => {
+    return allData.find(data => {
       return !this.isOutsideRange(date)
         ? moment(data.date).format('YYYY-MM-DD') === date.format('YYYY-MM-DD')
         : false
     })
+  }
+
+  isDayHighlighted = (date: moment.Moment, allData: eatOutData[]) => {
+    const eatOutData = this.getEatOutData(date, allData)
     return eatOutData ? eatOutData.will_eatout : false
+  }
+
+  renderDayContents = (date: moment.Moment, allData: eatOutData[]) => {
+    const eatOutData = this.getEatOutData(date, allData)
+    return eatOutData?.will_eatout === false ? (
+      <div className="CalendarDay__notEatout_calendar">{date.format('D')}</div>
+    ) : (
+      date.format('D')
+    )
   }
 
   isOutsideRange = (day: moment.Moment) => {
@@ -106,28 +119,48 @@ class App extends React.Component<AppProps, AppState> {
   render() {
     moment.lang('ja')
     return (
-      <DayPickerSingleDateController
-        date={null}
-        onDateChange={date =>
-          this.onDateChange(
-            date,
-            this.state.myData ? this.state.myData : this.props.dummyData
-          )
-        }
-        focused={true}
-        onFocusChange={() => {}}
-        isDayHighlighted={day =>
-          this.isDayHighlighted(
-            day,
-            this.state.myData ? this.state.myData : this.props.dummyData
-          )
-        }
-        monthFormat={'YYYY年M月'}
-        initialVisibleMonth={() => moment().locale('ja')}
-        weekDayFormat={'dd'}
-        isOutsideRange={this.isOutsideRange}
-        hideKeyboardShortcutsPanel
-      />
+      <div className="content">
+        <DayPickerSingleDateController
+          date={null}
+          onDateChange={date =>
+            this.onDateChange(
+              date,
+              this.state.myData ? this.state.myData : this.props.dummyData
+            )
+          }
+          focused={true}
+          onFocusChange={() => {}}
+          isDayHighlighted={day =>
+            this.isDayHighlighted(
+              day,
+              this.state.myData ? this.state.myData : this.props.dummyData
+            )
+          }
+          monthFormat={'YYYY年M月'}
+          initialVisibleMonth={() => moment().locale('ja')}
+          weekDayFormat={'dd'}
+          isOutsideRange={this.isOutsideRange}
+          renderDayContents={day =>
+            this.renderDayContents(
+              day,
+              this.state.myData ? this.state.myData : this.props.dummyData
+            )
+          }
+          renderCalendarInfo={() => (
+            <div className="calender-info">
+              <p>
+                <span className="color-box green" />
+                外で食べる（ご飯いらない）
+              </p>
+              <p>
+                <span className="color-box gray" />
+                家で食べる（ご飯いる）
+              </p>
+            </div>
+          )}
+          hideKeyboardShortcutsPanel
+        />
+      </div>
     )
   }
 }
